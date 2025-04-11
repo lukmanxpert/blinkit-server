583fe57f532533e528504d7b73ca2a2a16f54fb5
lukmanxpert <lukmanmiah2004@gmail.com>
2025-04-11 20:43:04 +0600

    blinkit server initialized

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet"
+
+dotenv.config();
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}))
+app.use(express.json());
+app.use(cookieParser())
+app.use(morgan())
+app.use(helmet({
+    crossOriginResourcePolicy: false
+}))
+const port = process.env.PORT || 9000
+
+
+// APIS
+
+app.get("/", (req, res)=>{
+    res.json({
+        message: "Server is running at port " + port
+    })
+})
+
+
+app.listen(port, ()=>{
+    console.log("Server is running at port", port);
+})
