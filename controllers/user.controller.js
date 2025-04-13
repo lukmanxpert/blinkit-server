c80d157193877e4936acb96799f1edff88f8a86f
lukmanxpert <lukmanmiah2004@gmail.com>
2025-04-13 14:33:03 +0600

    added user login controller, apis

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,8 @@ import userModel from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 import dotenv from "dotenv";
+import generateAccessToken from "../utils/generateAccessToken.js";
+import generateRefreshToken from "../utils/generateRefreshToken.js";
 dotenv.config();
 
 export async function registerUserController(req, res) {
@@ -81,11 +83,73 @@ export async function verifyEmailController(req, res) {
       message: "Verified email done",
       success: true,
       error: false,
-      data: updateUser
+      data: updateUser,
     });
   } catch (error) {
     res.status(500).json({
       message: error.message || error,
+    });
+  }
+}
+
+// login controller
+export async function loginController(req, res) {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Provide the valid email and password",
+        error: true,
+        success: false
+      })
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "User not registered",
+        error: true,
+        success: false,
+      });
+    }
+    if (user.status !== "Active") {
+      return res.status(400).json({
+        message: "Contact to Admin",
+        error: true,
+        success: false,
+      });
+    }
+    const checkPassword = await bcryptjs.compare(password, user.password);
+    if (!checkPassword) {
+      return res.status(400).json({
+        message: "Check your password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const accessToken = await generateAccessToken(user._id);
+    const refreshToken = await generateRefreshToken(user._id);
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+    res.cookie("accessToken", accessToken, cookiesOption);
+    res.cookie("refreshToken", refreshToken, cookiesOption);
+    return res.json({
+      message: "Login successfully",
+      error: false,
+      success: true,
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
     });
   }
 }
--- route/user.route.js
+++ route/user.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  loginController,
   registerUserController,
   verifyEmailController,
 } from "../controllers/user.controller.js";
@@ -8,4 +9,5 @@ const userRouter = Router();
 
 userRouter.post("/register", registerUserController);
 userRouter.post("/verify-email", verifyEmailController);
+userRouter.post("/login", loginController)
 export default userRouter;
