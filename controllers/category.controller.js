d3f7b7b8254a59d94d83a1d552ea6362a85faa21
lukmanxpert <lukmanmiah2004@gmail.com>
2025-05-01 23:53:38 +0600

    added category controller and add category controller

---
+++ controllers/category.controller.js
@@ -0,0 +1,38 @@
+import categoryModel from "../models/category.model";
+
+export const addCategoryController = async (req, res) => {
+  try {
+    const { name, image } = req.body;
+    if (!name || !image) {
+      return res.status.json({
+        message: "Enter require fields",
+        error: true,
+        success: false,
+      });
+    }
+    const addCategory = new categoryModel({
+      name,
+      image,
+    });
+    const saveCategory = await addCategory.save();
+    if (!saveCategory) {
+      return res.status(500).json({
+        message: "Category not created",
+        error: true,
+        success: false,
+      });
+    }
+    return res.json({
+      message: "Category Added",
+      data: saveCategory,
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
