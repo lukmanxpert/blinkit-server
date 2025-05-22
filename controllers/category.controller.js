2690c9bab1151f83ae940e01ee0555d6076c3831
lukmanxpert <lukmanmiah2004@gmail.com>
2025-05-22 23:45:29 +0600

    delete category controller added

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,6 @@
 import categoryModel from "../models/category.model.js";
+import productModel from "../models/product.model.js";
+import subCategoryModel from "../models/subCategory.model.js";
 
 // get category controller
 export const addCategoryController = async (req, res) => {
@@ -76,6 +78,51 @@ export const updateCategoryController = async (req, res) => {
       message: error.message || error,
       error: true,
       success: false,
+    });
+  }
+};
+
+// delete category controller
+export const deleteCategoryController = async (req, res) => {
+  try {
+    const { _id } = req.body;
+
+    const checkSubCategory = await subCategoryModel
+      .find({
+        category: {
+          $in: [_id],
+        },
+      })
+      .countDocuments();
+
+    const checkProducts = await productModel
+      .find({
+        category: {
+          $in: [_id],
+        },
+      })
+      .countDocuments();
+
+    if (checkSubCategory > 0 || checkProducts > 0) {
+      return res.status(400).json({
+        message: "Category is already in use.",
+        error: true,
+        success: false,
+      });
+    }
+
+    const result = await categoryModel.deleteOne({ _id });
+    res.json({
+      message: "Delete successfully",
+      success: true,
+      error: false,
+      data: result,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      success: false,
+      error: true,
     });
   }
 };
--- routes/category.route.js
+++ routes/category.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import auth from "../middleware/auth.js";
 import {
   addCategoryController,
+  deleteCategoryController,
   getCategoryController,
   updateCategoryController,
 } from "../controllers/category.controller.js";
@@ -11,5 +12,6 @@ const categoryRouter = Router();
 categoryRouter.post("/add-category", auth, addCategoryController);
 categoryRouter.get("/get", getCategoryController);
 categoryRouter.put("/update", auth, updateCategoryController);
+categoryRouter.delete("/delete", auth, deleteCategoryController);
 
 export default categoryRouter;
