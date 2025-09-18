c03cb654ec6102c632168db605e7535e096db740
lukmanxpert <lukmanmiah2004@gmail.com>
2025-09-19 04:05:25 +0600

    added checkout api

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 import cartProductModel from "../models/cartProduct.model.js";
 import orderModel from "../models/order.model.js";
 import userModel from "../models/user.model.js";
+import Stripe from "../config/stripe.js";
 
 export async function CashOnDeliveryOrderController(req, res) {
   try {
     const userId = req.userId; // auth middleware
     const { list_items, totalAmt, addressId, subTotalAmt } = req.body;
-    console.log('req.body :>> ', req.body);
+    console.log("req.body :>> ", req.body);
     const payload = list_items.map((el) => {
       return {
         userId: userId,
@@ -42,6 +43,68 @@ export async function CashOnDeliveryOrderController(req, res) {
       success: true,
       data: generatedOrder,
     });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export const priceWithDiscount = (price, dis) => {
+  const discountAmount = Math.ceil((Number(price) * Number(dis)) / 100);
+  const actualPrice = Number(price) - Number(discountAmount);
+  return actualPrice;
+};
+
+export async function paymentController(req, res) {
+  try {
+    const userId = req.userId; // auth middleware
+    const { list_items, totalAmt, addressId, subTotalAmt } = req.body;
+
+    const user = await userModel.findById(userId);
+
+    const line_items = list_items.map((item) => {
+      return {
+        price_data: {
+          currency: "bdt",
+          product_data: {
+            name: item.productId.name,
+            images: item.productId.image,
+            metadata: {
+              productId: item.productId._id,
+            },
+          },
+          unit_amount:
+            priceWithDiscount(item.productId.price, item.productId.discount) *
+            100,
+        },
+        adjustable_quantity: {
+          enabled: true,
+          minimum: 1,
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const params = {
+      submit_type: "pay",
+      mode: "payment",
+      payment_method_types: ["card"],
+      customer_email: user.email,
+      metadata: {
+        userId: userId,
+        addressId: addressId,
+      },
+      line_items: line_items,
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+    };
+
+    const session = await Stripe.checkout.sessions.create(params);
+
+    return res.status(200).json(session);
   } catch (error) {
     return res.status(500).json({
       message: error.message || error,
